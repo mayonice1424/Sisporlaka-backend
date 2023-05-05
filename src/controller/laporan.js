65ddb4df4b6552b5fcd0019c15113392d49e3d58
mayonice1424 <friyandrailham14@gmail.com>
2023-05-05 11:02:02 +0700

    pagination created

--- src/controller/laporan.js
+++ src/controller/laporan.js
@@ -1,10 +1,12 @@
 import { where } from "sequelize";
 import sequelize from "sequelize";
+import {Op} from "sequelize";
 import laporanModel from "../models/laporan.js";
+import laporanKategoriModel from "../models/laporanKategori.js";
 import kecamatanModel from "../models/kecamatanModel.js";
 
 export const createNewLaporan = async (req, res) => {
-  const { judul_kejadian, tanggal, waktu, lokasi, kerugian_materil, plat_ambulance, penyebab, keterangan } = req.body;
+  const { judul_kejadian, tanggal, waktu, lokasi, kerugian_materil, plat_ambulance, penyebab, keterangan,id_kecamatan } = req.body;
   try {
     const laporan = await laporanModel.create({
       judul_kejadian: judul_kejadian,
@@ -15,6 +17,7 @@ export const createNewLaporan = async (req, res) => {
       plat_ambulance: plat_ambulance,
       penyebab: penyebab,
       keterangan: keterangan,
+      id_kecamatan: id_kecamatan
     }).then((response) => {
     res.status(201).json({
       message: "Laporan berhasil dibuat",
@@ -25,9 +28,128 @@ export const createNewLaporan = async (req, res) => {
   }
 }
 
+export const getAllLaporanBySearch = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page)|| 0;
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search_query || '';
+    const offset = limit * page;
+    const totalRows = await laporanModel.count({
+      include: [kecamatanModel, laporanKategoriModel],
+      where: {
+        [Op.or]: [
+          {
+            judul_kejadian: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            tanggal: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            waktu: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            lokasi: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            plat_ambulance: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            penyebab: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            keterangan: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            '$Kecamatan.nama_kecamatan$': {
+              [Op.like]: `%${search}%`
+          }
+          }
+        ]
+      }
+    });
+    const totalPages = Math.ceil(totalRows / limit);
+    const laporan = await laporanModel.findAll({
+      include: [kecamatanModel, laporanKategoriModel],
+      order: [['id_laporan', 'DESC']],
+      where: {
+        [Op.or]: [
+          {
+            judul_kejadian: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            tanggal: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            waktu: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            lokasi: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            plat_ambulance: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            penyebab: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            keterangan: {
+              [Op.like]: `%${search}%`
+            }
+          },
+          {
+            '$Kecamatan.nama_kecamatan$': {
+              [Op.like]: `%${search}%`
+          }
+          }
+        ]
+      },
+      offset: offset,
+      limit: limit
+    });
+    res.status(200).json({
+       laporan: laporan,
+       page:page,
+       limit:limit,
+       totalRows:totalRows,
+       totalPages: totalPages
+       });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
 export const getAllLaporan = async (req, res) => {
   try {
-    const laporan = await laporanModel.findAll();
+    const laporan = await laporanModel.findAll({
+      include: [kecamatanModel, laporanKategoriModel],
+      order: [['id_laporan', 'DESC']]
+    });
     res.status(200).json({ laporan: laporan });
   } catch (error) {
     res.status(500).json({ error: error.message });
--- src/routes/laporan.js
+++ src/routes/laporan.js
@@ -1,11 +1,12 @@
-import {createNewLaporan,deleteLaporan,updateLaporan,getAllLaporan, getLaporanById, countLaporan, countLaporanByKecamatan } from "../controller/laporan.js";
+import {createNewLaporan,deleteLaporan,updateLaporan,getAllLaporanBySearch, getLaporanById, countLaporan, countLaporanByKecamatan, getAllLaporan } from "../controller/laporan.js";
 import express from 'express';
 const router = express.Router();
 
 router.post("/laporan", createNewLaporan);
 router.delete("/laporan/:id", deleteLaporan);
 router.patch("/laporan/:id", updateLaporan);
-router.get("/laporan", getAllLaporan);
+router.get("/laporan", getAllLaporanBySearch);
+router.get('/laporanall', getAllLaporan);
 router.get("/laporan/:id", getLaporanById);
 router.get('/count', countLaporan);
 router.get('/countkecamatan', countLaporanByKecamatan);

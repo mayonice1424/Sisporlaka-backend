f0f7b32f86c79eb0f5b6b203c227333f65368a9d
mayonice1424 <friyandrailham14@gmail.com>
2023-07-05 03:20:10 +0700

    menambahkan cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors(
     {
         credentials: true,
-        origin: `http://localhost:3000`
+        origin: `https://sisporlaka.iterahero.com`
     }
 ))
 app.use(cookieParser())
